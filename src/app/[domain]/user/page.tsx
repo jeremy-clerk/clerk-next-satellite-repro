eb2fb0c4c9e0a013331c23163fd423b6c6e73194
Jeremy Wright <jeremy@clerk.dev>
2024-12-12 00:58:20 +0530

    testing currentuser

--- src/app/[domain]/user/page.tsx
+++ src/app/[domain]/user/page.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
+import {currentUser} from "@clerk/nextjs/server";
 
-export default function Page(){
+export default async function Page(){
+    const user = await currentUser();
 
      return(
          <div>
-         <p>
-             Hello
-         </p>
+         <h1>
+             Hello, {user?.firstName}
+         </h1>
              <Link href={"/dashboard"} >Dashboard</Link>
          </div>
      )
