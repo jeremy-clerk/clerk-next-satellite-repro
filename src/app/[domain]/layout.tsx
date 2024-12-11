1519bc4b3d247e2abc32a48b59962c77d6706868
Jeremy Wright <jeremy@clerk.dev>
2024-12-12 01:21:15 +0530

    wip

--- src/app/[domain]/layout.tsx
+++ src/app/[domain]/layout.tsx
@@ -23,6 +23,7 @@ export default async function RootLayout({
             allowedRedirectOrigins={[process.env.NEXT_PUBLIC_SATELLITE_ROOT_DOMAIN as string]}
             domain={isSatellite ? process.env.NEXT_PUBLIC_SATELLITE_ROOT_DOMAIN as string : process.env.NEXT_PUBLIC_ROOT_DOMAIN as string}
             isSatellite={isSatellite}
+            afterSignOutUrl={(process.env.NEXT_PUBLIC_ROOT_DOMAIN as string).concat("/login")}
         >
             <html lang="en">
             <body>
